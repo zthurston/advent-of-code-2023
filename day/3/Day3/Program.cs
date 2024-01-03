2544cc8b0dbd6064fd94b1f5af5e37d3480bd3dc
Zach Thurston <zach@zthurston.com>
2024-01-02 21:05:04 -0700

    Play with ReadOnlySpan

--- day/3/Day3/Program.cs
+++ day/3/Day3/Program.cs
@@ -10,7 +10,6 @@ using System.Text.RegularExpressions;
 //string[] allLines = await File.ReadAllLinesAsync("./test_input.txt");
 string[] allLines = await File.ReadAllLinesAsync("./input.txt");
 
-
 static bool IsNonSymbolChar(char currentChar)
 {
     return char.IsDigit(currentChar) || currentChar == '.' || char.IsControl(currentChar);
@@ -20,48 +19,51 @@ Dictionary<int, List<Match>> matches = new();
 
 // this is a symbol centric solution - look in each line for symbols, then look around the symbol for digits, if digits are found, process out into a number
 // and create a Find object. After we have processed all lines, we can add up the found numbers and print the result
-for (int i = 0; i < allLines.Length; i++)
+void ProcessFile()
 {
-
-    Console.WriteLine($"processing line: {i + 1}: {allLines[i]}");
-    string current = allLines[i];
-    string? previousLine = i > 0 ? allLines[i - 1] : null;
-    string? nextLine = i < allLines.Length - 1 ? allLines[i + 1] : null;
-    int lineNumber = i + 1;
-
-    // idea, look through each char of the string, when found, look around the character
-    // for digits, and if found, grab that number. Use a found list to avoid double counting
-    for (int j = 0; j < current.Length; j++)
+    for (int i = 0; i < allLines.Length; i++)
     {
-        // could do a regex and iterate over matches to avoid iterating over the string
-        char currentChar = current[j];
 
-        // check if currentChar is a digit, a period, or a newline, if so, continue to next char
-        if (IsNonSymbolChar(currentChar))
+        Console.WriteLine($"processing line: {i + 1}: {allLines[i]}");
+        string current = allLines[i];
+        string? previousLine = i > 0 ? allLines[i - 1] : null;
+        string? nextLine = i < allLines.Length - 1 ? allLines[i + 1] : null;
+        int lineNumber = i + 1;
+
+        // idea, look through each char of the string, when found, look around the character
+        // for digits, and if found, grab that number. Use a found list to avoid double counting
+        for (int j = 0; j < current.Length; j++)
         {
-            //Console.WriteLine($"ignoring char: {currentChar}");
-            continue;
-        }
-        //Console.WriteLine($"found symbol: {currentChar} at line: {i + 1} position: {j + 1}");
-        
-        // found a symbol, look around it for digits
-        // probably could use Span<char> here to avoid allocations
-        string currentSlice = current.Substring(j - 1, 3);
-        ProcessForMatches(currentSlice, current, lineNumber, j);
-        var previousSlice = previousLine?.Substring(j - 1, 3);
-        if (previousSlice != null && previousLine != null)
-        {
-            ProcessForMatches(previousSlice, previousLine, lineNumber - 1, j);
-        }
-        var nextSlice = nextLine?.Substring(j - 1, 3);
-        if (nextSlice != null && nextLine != null)
-        {
-            ProcessForMatches(nextSlice, nextLine, lineNumber + 1, j);
+            // could do a regex and iterate over matches to avoid iterating over the string
+            char currentChar = current[j];
+
+            // check if currentChar is a digit, a period, or a newline, if so, continue to next char
+            if (IsNonSymbolChar(currentChar))
+            {
+                //Console.WriteLine($"ignoring char: {currentChar}");
+                continue;
+            }
+            //Console.WriteLine($"found symbol: {currentChar} at line: {i + 1} position: {j + 1}");
+
+            // found a symbol, look around it for digits
+            // this algorithm is very brute force in 
+            var currentSlice = current.AsSpan().Slice(j - 1, 3);
+            ProcessForMatches(currentSlice, current, lineNumber, j);
+            var previousSlice = previousLine?.Substring(j - 1, 3);
+            if (previousSlice != null && previousLine != null)
+            {
+                ProcessForMatches(previousSlice, previousLine, lineNumber - 1, j);
+            }
+            var nextSlice = nextLine?.Substring(j - 1, 3);
+            if (nextSlice != null && nextLine != null)
+            {
+                ProcessForMatches(nextSlice, nextLine, lineNumber + 1, j);
+            }
         }
     }
 }
 
-void ProcessForMatches(string slice, string line, int lineNumber, int symbolIndex)
+void ProcessForMatches(ReadOnlySpan<char> slice, string line, int lineNumber, int symbolIndex)
 {
     // see if the slice has digits, if not, return
     if (!HasDigit(slice))
@@ -73,6 +75,7 @@ void ProcessForMatches(string slice, string line, int lineNumber, int symbolInde
 
     // the slice has digits, so this line has some numbers that are part numbers
     // match strings that consist only of numbers
+    // would be nice to constrain where we're looking instead of looking at the whole string
     string pattern = @"\d+";
 
     foreach (var match in Regex.Matches(line, pattern).Cast<Match>())
@@ -111,6 +114,8 @@ void AddMatchIfNotAlreadyAdded(Match match, int lineNumber)
     matches[lineNumber].Add(match);
 }
 
+ProcessFile();
+
 Console.WriteLine("printing finds");
 int sum = 0;
 foreach (var matchPair in matches)
@@ -122,10 +127,18 @@ foreach (var matchPair in matches)
         Console.WriteLine($"value: {lineMatches.Value} line: {matchPair.Key} start position: {lineMatches.Index}");
     }
 }
+ProcessFile();
 // answer was 509115
 Console.WriteLine($"sum: {sum}");
 
-bool HasDigit(string input)
+bool HasDigit(ReadOnlySpan<char> input)
 {
-    return input.Any(c => char.IsDigit(c));
+    foreach (char c in input)
+    {
+        if (char.IsDigit(c))
+        {
+            return true;
+        }
+    }
+    return false;
 }
